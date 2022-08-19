20566759258ac27afe5db19bc6d411d812aa777e
OksMor <Sancho_2006_ru@mail.ru>
2022-08-19 11:02:30 +0300

    добавляет заглушку при отсутствии точек маршрута

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -1,31 +1,34 @@
+import HeaderInfoView from '../view/header-info-view.js';
+import HeaderFiltersView from '../view/header-filters-view.js';
 import TripSortView from '../view/trip-sort-view.js';
 import TripEventsListView from '../view/trip-events-list-view.js';
 import EditPointView from '../view/edit-point-view.js';
 import TripItemCardView from '../view/trip-item-card-view.js';
+import NoPointView from '../view/no-point-view.js';
 
 import { render } from '../render.js';
 
-export default class TripPresenter {
+export default class MainPresenter {
+  #infoContainer = null;
+  #filterContainer = null;
   #tripContainer = null;
+  #pointsModel = null;
   #tripList = new TripEventsListView();
   #tripPoints = [];
-  #pointsModel = null;
 
-  init = ( tripContainer, pointsModel ) => {
+  constructor( infoContainer, filterContainer, tripContainer, pointsModel ) {
+    this.#infoContainer = infoContainer;
+    this.#filterContainer = filterContainer;
     this.#tripContainer = tripContainer;
-
     this.#pointsModel = pointsModel;
+  }
+
+  init = () => {
     this.#tripPoints = [...this.#pointsModel.points];
 
-    // console.log(this.tripPoint);
-
-    render( new TripSortView(), this.#tripContainer );
-    render( this.#tripList, this.#tripContainer );
-
-    for ( let i = 0; i < this.#tripPoints.length; i++ ) {
-      this.#renderPoint( this.#tripPoints[i] );
-    }
+    this.#renderEvent();
   };
+
 
   #renderPoint = (point) => {
     const pointComponent = new TripItemCardView(point);
@@ -63,5 +66,22 @@ export default class TripPresenter {
     });
 
     render(pointComponent, this.#tripList.element);
+  };
+
+  #renderEvent = () => {
+    render( new HeaderFiltersView(), this.#filterContainer );
+    if (this.#tripPoints.length === 0) { // завязка на фильтрацию
+      render( new NoPointView(), this.#tripContainer);
+    } else {
+      render( new HeaderInfoView(), this.#infoContainer, 'afterbegin' );
+
+
+      render( new TripSortView(), this.#tripContainer );
+      render( this.#tripList, this.#tripContainer );
+
+      for ( let i = 0; i < this.#tripPoints.length; i++ ) {
+        this.#renderPoint( this.#tripPoints[i] );
+      }
+    }
   };
 }
